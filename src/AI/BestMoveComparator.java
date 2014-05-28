ca4fb51931a7c58ee2cb8817a3e9c705aac95925
joshlava <josh.juve@live.com>
2014-05-28 13:46:03 +0800

    Update BestMoveComparator.java

--- src/AI/BestMoveComparator.java
+++ src/AI/BestMoveComparator.java
@@ -5,25 +5,22 @@ import java.util.Comparator;
 public class BestMoveComparator implements Comparator<Node>{
 
 	public int compare(Node o1, Node o2) {
-	/*	if(o1.whiteSpace>5){
-			if(o1.score<o2.score)
-				return 1;
-			else 
-				return -1;
-		}else*/
-		if(o1.whiteSpace==o2.whiteSpace){
-			if(o1.score>o2.score)
-				return -1;
-			else 
-				return 1;
-		}
-		if(o1.whiteSpace < o2.whiteSpace)
-			return 1;
-		else if(o1.whiteSpace > o2.whiteSpace)
+
+		if(o1.big > o2.big){
 			return -1;
-		else return 0;
+		} else if(o1.big == o2.big){
+			if(o1.score > o2.score){
+				return -1;
+			} else if(o1.score == o2.score){
+				if(o1.whiteSpace > o2.whiteSpace){
+					return -1;
+				} else if(o2.whiteSpace == o1.whiteSpace){
+					return 0;
+				} else return 1;
+			} else
+				return 1;
+		} else
+			return 1;
 	}
-
-
 
 }
