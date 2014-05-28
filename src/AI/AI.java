f8d76a420bd9fd1a7dfb1fff1e8459ad32e045be
joshlava <josh.juve@live.com>
2014-05-28 13:46:31 +0800

    Update AI.java

--- src/AI/AI.java
+++ src/AI/AI.java
@@ -12,30 +12,29 @@ import java.util.PriorityQueue;
 public class AI {
 
 	private ArrayDeque<String> AStar(int[][] board) {
-<<<<<<< HEAD
-		int z = 15;
-=======
-		int z = 6;
->>>>>>> fbacf704b75b3a5db744f76d04e9df5c72b33677
+		int z = 1000;
+		//int z = 6;
+
 		Comparator<Node> comparator = new BestMoveComparator();
 		PriorityQueue<Node> openList = new PriorityQueue<Node>(z, comparator);
-		ArrayDeque<Node> closedList = new ArrayDeque<Node>();
+		PriorityQueue<Node> closedList = new PriorityQueue<Node>(z, comparator);
 		GUI.Threes.readFile();
 		GUI.Threes.set();
 		ArrayDeque<String> initialMoves = new ArrayDeque<String>();
 		// initialMoves.add(" ");
 		Node initial = new Node(Moves.CountWhite(GUI.Threes.board),
-				Moves.countScore(GUI.Threes.board), initialMoves);
+				Moves.countScore(GUI.Threes.board), Moves.getBig(GUI.Threes.board), initialMoves);
 		openList.add(initial); // add initial board to openList
 
 		ArrayDeque<String> moves = new ArrayDeque<String>();
 		int whiteSpace = 0;
 		int Score = 0;
+		int big = 0;
 		int goal = 10;
 		int i = 0;
 		while (!openList.isEmpty() && i < z) {
-			Node node = openList.element();
-			if (!closedList.contains(node)) {
+			Node node = openList.remove();
+
 				closedList.add(node);
 				/*
 				 * System.out.println("/=/=/=/=/==/=/=/=/=/=/==");
@@ -56,8 +55,8 @@ public class AI {
 				node.moves.offer("L");
 				temp = Moves.doMove(board, node.moves);
 				int tempint = (Integer) temp.get(1);
-				Node left = new Node(tempint, (Integer) temp.get(0),
-						node.moves.clone());
+				Node left = new Node(tempint, (Integer) temp.get(0),(Integer) temp.get(2)
+						, node.moves.clone());
 				openList.add(left);
 				node.moves.removeLast();
 				System.out.println("Whitespace is: " + (Integer)temp.get(1));
@@ -65,7 +64,7 @@ public class AI {
 				node.moves.add("R");
 				temp = Moves.doMove(board, node.moves);
 				Node right = new Node((Integer) temp.get(1),
-						(Integer) temp.get(0), node.moves.clone());
+						(Integer) temp.get(0),(Integer) temp.get(2), node.moves.clone());
 				openList.add(right);
 				node.moves.removeLast();
 				System.out.println("Whitespace is: " + (Integer)temp.get(1));
@@ -73,7 +72,7 @@ public class AI {
 				node.moves.add("D");
 				temp = Moves.doMove(board, node.moves);
 				Node down = new Node((Integer) temp.get(1),
-						(Integer) temp.get(0), node.moves.clone());
+						(Integer) temp.get(0), (Integer)temp.get(2), node.moves.clone());
 				openList.add(down);
 				node.moves.removeLast();
 				System.out.println("Whitespace is: " + (Integer)temp.get(1));
@@ -81,16 +80,19 @@ public class AI {
 				node.moves.offer("U");
 				temp = Moves.doMove(board, node.moves);
 				Node up = new Node((Integer) temp.get(1),
-						(Integer) temp.get(0), node.moves.clone());
+						(Integer) temp.get(0), (Integer)temp.get(2), node.moves.clone());
 				openList.add(up);
 				node.moves.removeLast();
 				System.out.println("Whitespace is: " + (Integer)temp.get(1));
 
 				// }
-			}
 
 			i++;
 		}
+		Node node = closedList.remove();
+		whiteSpace = node.whiteSpace;
+		Score = node.score;
+		moves = node.moves;
 		String whitescore = "White Space Score is: " + whiteSpace;
 		String scorescore = "Game Score is: " + Score;
 		moves.addFirst(whitescore);
